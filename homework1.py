a59ea65f5213b4040adf712e399354a62d26e1c6
Your Name <you@example.com>
2017-12-01 22:45:10 +0800

    first commit

---
+++ homework1.py
@@ -0,0 +1,4 @@
+for a in range(1,10):
+    for b in range(1,a+1):
+        print('%s * %s = %s ' %(a,b,a*b)),
+    print('\n')
---
+++ homework2.py
@@ -0,0 +1,9 @@
+#/bin/env python
+# coding: utf-8
+list = [1,2,3,2,12,3,1,3,21,2,2,3,4111,22,444,111,3333,4,5,777,65535,45,5001,33,45,6000] 
+max = 0
+for a in list:
+    if a > max:
+        max = a
+print (max)
+            
